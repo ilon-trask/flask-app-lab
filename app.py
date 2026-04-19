306bf7df93cd178cbf6ad75b29a5730f48cf51e8
myha <misha007zty@gmail.com>
2026-04-19 17:15:22 +0200

    lab 2

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def main():
-    return "hello world"
+def resume():
+    return render_template("resume.html")
+
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
 
 
 if __name__ == "__name__":
